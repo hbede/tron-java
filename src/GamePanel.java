851b6a6845288ea67187e0820b4bafd085220a68
Benedek Horváth <71231495+hbede@users.noreply.github.com>
2021-11-10 13:57:30 +0100

    Added attributes and empty methods to class GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+    private static int RES_X;
+    private static int RES_Y;
+    private static int UNIT_SIZE;
+    private boolean isRunning;
+    private Timer timer;
+
+    public void start() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void end() {
+        
+    }
+
 }
